34429fb393be2f2484585bee067249f1333223d8
Fahad Iqbal <fahadiqbal9318@gmail.com>
2025-01-25 03:52:41 +0500

    Commit - Validate user inputs and refactor calculate_monthly_payment method.

--- main.py
+++ main.py
@@ -1,24 +1,45 @@
-# Inputs: 
-# 50000
-# 5%
-# 10
+# Input validation for Loan Amount
+while True:
+    try:
+        loan_amount = float(input("Enter loan amount: "))
+        if loan_amount > 0:
+            break
+        print("Invalid! Loan amount must be positive.")
+    except ValueError:
+        print("Invalid input! Please enter a number.")
 
-# Outputs:
-# $530.33
+# Input validation for Annual Interest Rate
+while True:
+    try:
+        annual_interest_rate = float(input("Enter annual interest rate (%): "))
+        if annual_interest_rate >= 0:
+            break
+        print("Invalid! Interest rate can't be negative.")
+    except ValueError:
+        print("Invalid input! Please enter a number")
 
-# 1. Take Inputs
-loan_amount = float(input("Enter loan amount: "))
-annual_interest_rate = float(input("Enter annual interest rate (%): "))
-loan_terms_years = float(input("Enter loan terms in (years): "))
+# Input validation for Loan Term Years
+while True:
+    try:
+        loan_terms_years = float(input("Enter loan terms in (years): "))
+        if loan_terms_years >= 1 and loan_terms_years <= 50:
+            break
+        print("Invalid! Loan term must be atleast 1 year or utmost 50 years")
+    except ValueError:
+        print("Invalid input! Please enter a number")
 
-def calculate_monthly_payment():
-    # Convert monthly rate
-    monthly_rate = annual_interest_rate / 12 / 100
-    # Calculate total number of payments
-    number_of_payments = loan_terms_years * 12
+# Convert monthly rate
+monthly_rate = annual_interest_rate / 12 / 100
+# Calculate total number of payments
+number_of_payments = loan_terms_years * 12
+
+def calculate_monthly_payment(P, r, n):             # P = Loan Amount, r = Monthly Rate, n = Total Numbers of Payments
     # Calculate monthly payment
-    monthly_payment = (loan_amount * monthly_rate * (1 + monthly_rate)**number_of_payments) / ((1 + monthly_rate)**number_of_payments - 1)
-    print(f"Your monthly payment is ${round(monthly_payment, 3)}")
+    if monthly_rate == 0:
+        return loan_amount / number_of_payments
+    else:
+        return (loan_amount * monthly_rate * (1 + monthly_rate)**number_of_payments) / ((1 + monthly_rate)**number_of_payments - 1)
 
-
-calculate_monthly_payment()
+# Compute and display the monthly payment
+monthly_payment = calculate_monthly_payment(P= loan_amount, r= monthly_rate, n= number_of_payments)
+print(f"Your monthly payment is ${round(monthly_payment, 2)}")
